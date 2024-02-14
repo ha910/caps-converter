a016803b11d1b38e586f688943130c23964ac842
ha910 <hrryalbrt1021@gmail.com>
2024-02-14 10:23:14 -0500

    remove login, add lowercase function

--- script.js
+++ script.js
@@ -1,36 +1,57 @@
 "use strict";
 
-const acceptedUsers = [
-  "guest1",
-  "halbert",
-  "jlaukkanen",
-  "sklaubert",
-  "cvarelas",
-  "pdavis",
-  "jplaisted",
-  "glizotte",
-  "ecollazo",
-  "kregan",
-  "ksawin",
-  "mhudroge",
-  "kbermudez-cotto",
-  "hdoyle",
-];
+// const acceptedUsers = [
+//   "guest1",
+//   "halbert",
+//   "jlaukkanen",
+//   "sklaubert",
+//   "cvarelas",
+//   "pdavis",
+//   "jplaisted",
+//   "glizotte",
+//   "ecollazo",
+//   "kregan",
+//   "ksawin",
+//   "mhudroge",
+//   "kbermudez-cotto",
+//   "hdoyle",
+// ];
 
-function checkUsername() {
-  const username = document
-    .querySelector(".signin input[type='text']")
-    .value.toLowerCase();
-  if (acceptedUsers.includes(username)) {
-    sessionStorage.setItem("username", username);
-    document.querySelector(".signin").style.display = "none";
-    document.querySelector(".box").style.display = "flex";
-    document.querySelector(".guest").style.display = "none";
-    sessionStorage.setItem("signedIn", "true");
-  } else {
-    alert("Invalid username. Please try again.");
-  }
-}
+// function checkUsername() {
+//   const username = document
+//     .querySelector(".signin input[type='text']")
+//     .value.toLowerCase();
+//   if (acceptedUsers.includes(username)) {
+//     sessionStorage.setItem("username", username);
+//     document.querySelector(".signin").style.display = "none";
+//     document.querySelector(".box").style.display = "flex";
+//     document.querySelector(".guest").style.display = "none";
+//     sessionStorage.setItem("signedIn", "true");
+//   } else {
+//     alert("Invalid username. Please try again.");
+//   }
+// }
+
+// const savedUsername = sessionStorage.getItem("username");
+// if (acceptedUsers.includes(savedUsername)) {
+//   document.querySelector(".signin").style.display = "none";
+//   document.querySelector(".box").style.display = "flex";
+//   document.querySelector(".signin input[type='text']").value = savedUsername;
+//   sessionStorage.setItem("signedIn", "true");
+// }
+
+// document
+//   .querySelector(".signin input[type='text']")
+//   .addEventListener("keydown", function (event) {
+//     if (event.key === "Enter") {
+//       checkUsername();
+//     }
+//   });
+
+// const signedIn = sessionStorage.getItem("signedIn");
+// if (signedIn === "true") {
+//   document.querySelector(".guest").style.display = "none";
+// }
 
 document.querySelector(".copy").addEventListener("click", function () {
   const output = document.querySelector(".output");
@@ -52,21 +73,13 @@ function convertToCaps() {
   input.value = "";
 }
 
-const savedUsername = sessionStorage.getItem("username");
-if (acceptedUsers.includes(savedUsername)) {
-  document.querySelector(".signin").style.display = "none";
-  document.querySelector(".box").style.display = "flex";
-  document.querySelector(".signin input[type='text']").value = savedUsername;
-  sessionStorage.setItem("signedIn", "true");
+function convertToLower() {
+  const input = document.querySelector(".box input[type='text']");
+  const output =
+    input.value.charAt(0).toUpperCase() + input.value.slice(1).toLowerCase();
+  document.querySelector(".box textarea").value = output;
+  input.value = "";
 }
-
-document
-  .querySelector(".signin input[type='text']")
-  .addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      checkUsername();
-    }
-  });
 
 document
   .querySelector(".box input[type='text']")
@@ -75,8 +88,3 @@ document
       convertToCaps();
     }
   });
-
-const signedIn = sessionStorage.getItem("signedIn");
-if (signedIn === "true") {
-  document.querySelector(".guest").style.display = "none";
-}
